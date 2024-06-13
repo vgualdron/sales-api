61a71cc411a29b0c112be4d1ad98739f38e5b7b1
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-06-13 02:09:11 -0500

    add image controller

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -21,7 +21,7 @@ class ImageController extends Controller
             $image_avatar_b64 = $request->image;
             $img = $this->getB64Image($image_avatar_b64);
             // Obtener la extensión de la Imagen
-            //$img_extension = $this->getB64Extension($image_avatar_b64);
+            $img_extension = $this->getB64Extension($image_avatar_b64);
             // Crear un nombre aleatorio para la imagen
             // $img_name = strtotime("now") . '.' . $img_extension;
             // echo $image_name;
