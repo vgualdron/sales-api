7409475c898b1bd4e67834908bdb73bef146124e
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-06-24 19:10:50 -0500

    add Zip controller

--- app/Services/Implementations/ZipServiceImplement.php
+++ app/Services/Implementations/ZipServiceImplement.php
@@ -35,8 +35,8 @@
                     return response()->json([
                         'message' => [
                             [
-                                'text' => 'No hay zonas para mostrar',
-                                'detail' => 'Aun no ha registrado ninguna zona'
+                                'text' => 'No hay zip para mostrar',
+                                'detail' => 'Aun no ha registrado ningun zip'
                             ]
                         ]
                     ], Response::HTTP_NOT_FOUND);
@@ -45,7 +45,7 @@
                 return response()->json([
                     'message' => [
                         [
-                            'text' => 'Se ha presentado un error al cargar las zonas',
+                            'text' => 'Se ha presentado un error al cargar lo zips',
                             'detail' => 'intente recargando la página'
                         ]
                     ]
@@ -54,22 +54,22 @@
         }
 
 
-        function create(array $zone){
+        function create(array $zip){
             try {
-                $validation = $this->validate($this->validator, $zone, null, 'registrar', 'zona', null);
+                $validation = $this->validate($this->validator, $zip, null, 'registrar', 'zip', null);
                 if ($validation['success'] === false) {
                     return response()->json([
                         'message' => $validation['message']
                     ], Response::HTTP_BAD_REQUEST);
                 }
                 $status = $this->zone::create([
-                    'code' => $zone['code'],
-                    'name' => $zone['name']
+                    'code' => $zip['code'],
+                    'name' => $zip['name']
                 ]);
                 return response()->json([
                     'message' => [
                         [
-                            'text' => 'Zona registrada con éxito',
+                            'text' => 'Zip registrada con éxito',
                             'detail' => null
                         ]
                     ]
@@ -78,127 +78,7 @@
                 return response()->json([
                     'message' => [
                         [
-                            'text' => 'Advertencia al registrar la zona',
-                            'detail' => 'Si este problema persiste, contacte con un administrador'
-                        ]
-                    ]
-                ], Response::HTTP_INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        function update(array $zone, int $id){
-            try {
-                $validation = $this->validate($this->validator, $zone, $id, 'actualizar', 'zona', null);
-                if ($validation['success'] === false) {
-                    return response()->json([
-                        'message' => $validation['message']
-                    ], Response::HTTP_BAD_REQUEST);
-                }
-                $sql = $this->zone::find($id);
-                if(!empty($sql)) {
-                    $sql->name = $zone['name'];
-                    $sql->code = $zone['code'];
-                    $sql->save();
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'Zona actualizada con éxito',
-                                'detail' => null
-                            ]
-                        ]
-                    ], Response::HTTP_OK);
-                } else {
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'Advertencia al actualizar la zona',
-                                'detail' => 'La zona no existe'
-                            ]
-                        ]
-                    ], Response::HTTP_NOT_FOUND);
-                }
-            } catch (\Throwable $e) {
-                return response()->json([
-                    'message' => [
-                        [
-                            'text' => 'Advertencia al actualizar la zona',
-                            'detail' => 'Si este problema persiste, contacte con un administrador'
-                        ]
-                    ]
-                ], Response::HTTP_INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        function delete(int $id){   
-            try {
-                $sql = $this->zone::find($id);
-                if(!empty($sql)) {
-                    $deleted = $sql->delete();
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'Zona eliminada con éxito',
-                                'detail' => null
-                            ]
-                        ]
-                    ], Response::HTTP_OK);
-                } else {
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'Advertencia al eliminar la zona',
-                                'detail' => 'La zona no existe'
-                            ]
-                        ]
-                    ], Response::HTTP_NOT_FOUND);
-                }
-            } catch (\Throwable $e) {
-                if ($e->getCode() !== "23000") {
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'Advertencia al eliminar la zona',
-                                'detail' => 'Si este problema persiste, contacte con un administrador'
-                            ]
-                        ]
-                    ], Response::HTTP_INTERNAL_SERVER_ERROR);
-                } else {
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'No se permite eliminar el registro',
-                                'detail' => 'La zona se encuentra asociada a otro registro'
-                            ]
-                        ]
-                    ], Response::HTTP_INTERNAL_SERVER_ERROR);
-                }
-            }
-        }
-
-        function get(int $id){
-            try {
-                $sql = $this->zone::select('id', 'code', 'name')
-                            ->where('id', $id)   
-                            ->first();
-                if(!empty($sql)) {
-                    return response()->json([
-                        'data' => $sql
-                    ], Response::HTTP_OK);
-                } else {
-                    return response()->json([
-                        'message' => [
-                            [
-                                'text' => 'La zona no existe',
-                                'detail' => 'por favor recargue la página'
-                            ]
-                        ]
-                    ], Response::HTTP_NOT_FOUND);
-                }
-            } catch (\Throwable $e) {
-                return response()->json([
-                    'message' => [
-                        [
-                            'text' => 'Se ha presentado un error al buscar la zona',
+                            'text' => 'Advertencia al registrar el zip',
                             'detail' => 'Si este problema persiste, contacte con un administrador'
                         ]
                     ]
