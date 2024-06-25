102605fc826794e24c4570f5563f323b0c256e9a
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-06-24 21:10:20 -0500

    add Zip controller 10

--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\Implementations\ZipServiceImplement;
+use ZipArchive;
+use File;
 
 class ZipController extends Controller
 {
