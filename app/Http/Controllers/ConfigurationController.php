e555718e25ac4501591d0d8c08479303caab2dde
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-14 15:13:59 -0500

    add fields

--- app/Http/Controllers/ConfigurationController.php
+++ app/Http/Controllers/ConfigurationController.php
@@ -12,7 +12,20 @@ class ConfigurationController extends Controller
 {
     public function index(Request $request)
     {
-        echo "asda";
+        try {
+            $idUserSesion = $request->user()->id;
+            $items = Configuration::where('id', '>', 0)->get();
+        } catch (Exception $e) {
+            return response()->json([
+                'data' => [],
+                'message'=> $idUserSesion,
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'data' => $items,
+            'message' => 'Succeed',
+        ], JsonResponse::HTTP_OK);
     }
 
     public function show(Request $request, $id)
