c424e07b36df8ee033cfe8ce4b4cf94ffbabe02f
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-14 14:10:55 -0500

    add fields

--- app/Models/Configuration.php
+++ app/Models/Configuration.php
@@ -1,42 +1,22 @@
 <?php
+
 namespace App\Models;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class Configuration extends Authenticatable
+use Illuminate\Database\Eloquent\Model;
+
+class Configuration extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    protected $guarded = ['id'];
+    public $table = "configurations";
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'reference',
         'value',
         'user_id'
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
