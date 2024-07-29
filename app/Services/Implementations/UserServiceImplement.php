04dd13592eefde9acaaeff377fd5db09110861c8
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-07-29 03:48:12 -0500

    add push token

--- app/Services/Implementations/UserServiceImplement.php
+++ app/Services/Implementations/UserServiceImplement.php
@@ -77,6 +77,7 @@
                                 'u.name as name',
                                 'u.phone as phone',
                                 'r.name as role',
+                                'u.push_token as pushToken',
                                 DB::Raw('IF(u.active = 1, "ACTIVO", "NO ACTIVO") as status'),
                                 DB::Raw('IF(u.yard IS NOT NULL, y.name, "Sin sector asignado") as yard'),
                                 DB::Raw('IF(y.zone IS NOT NULL, z.name, "Sin ciudad asignada") as zone')
