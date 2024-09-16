a2fba96e3a4a4a24a985cf15276a55ac3e9e4ebb
Victor Gualdron Ruiz <vgualdron@estratec.local>
2024-09-16 12:02:57 -0500

    add func reports

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,14 @@ return [
             'throw' => false,
         ],
 
+        'payments' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/payments'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
