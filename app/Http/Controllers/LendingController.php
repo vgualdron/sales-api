94d08a272c0b76d940bb35e6ed795c5e07da67e6
Victor Gualdron Ruiz <vgualdron@estratec.local>
2024-09-16 07:03:43 -0500

    add func reports

--- app/Http/Controllers/LendingController.php
+++ app/Http/Controllers/LendingController.php
@@ -9,6 +9,7 @@ use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class LendingController extends Controller
 {
