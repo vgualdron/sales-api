730cfdd756384cf311be4baa362a5ad8bef9c896
vgualdron <victor.gualdron.r@gmail.com>
2024-11-16 13:09:10 -0500

    resolve adjustments prestamos

--- app/Http/Controllers/LendingController.php
+++ app/Http/Controllers/LendingController.php
@@ -81,6 +81,7 @@ class LendingController extends Controller
             ])
             ->leftJoin('payments', 'lendings.id', '=', 'payments.lending_id')
             ->leftJoin('news', 'news.id', '=', 'lendings.new_id')
+            ->with('payments')
             ->where(function ($query) use ($idList, $status1, $status2, $status3, $startDate, $endDate) {
                 $query->where(function ($subQuery) use ($idList, $status1) {
                     $subQuery->where('listing_id', $idList)
