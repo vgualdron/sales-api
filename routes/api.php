4c2fc7c92f4048e267e344f5d718b715e3091259
vgualdron <victor.gualdron.r@gmail.com>
2024-11-18 21:09:44 -0500

    add func questions

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use App\Http\Controllers\{
                         ExpenseController,
                         AreaController,
                         ItemController,
+                        QuestionController,
                     };
 
 /*
