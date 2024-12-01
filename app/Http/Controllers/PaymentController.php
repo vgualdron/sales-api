065567e8f2724524599cdc6bd459bb18765c5b33
vgualdron <victor.gualdron.r@gmail.com>
2024-11-30 23:19:47 -0500

    nequi fix

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -20,9 +20,11 @@ class PaymentController extends Controller
                                 'lendings.nameDebtor',
                                 'listings.name as listName',
                                 'users.name as userName',
+                                'nequis.name as nequiName',
                                 'files.url as urlFile',
-                                'files.type as typeFile',
-                            )->leftjoin('files', 'files.id', 'payments.file_id')
+                                'files.type as typeFile')
+                            ->leftjoin('files', 'files.id', 'payments.nequi')
+                            ->leftjoin('nequis', 'nequis.id', 'payments.file_id')
                             ->join('lendings', 'lendings.id', 'payments.lending_id')
                             ->join('listings', 'listings.id', 'lendings.listing_id')
                             ->join('users', 'users.id', 'listings.user_id_collector')
