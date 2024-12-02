912335d451e317c42a0279e768420ca4e04b7707
vgualdron <victor.gualdron.r@gmail.com>
2024-12-02 00:40:56 -0500

    work on delivery route

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -192,7 +192,7 @@ class ListingController extends Controller
       
             $itemList = Listing::find($idList);
 
-            $itemTransfer = Payment::selectRaw('COUNT(*) as total_count, SUM(payments.amount) as total_amount, COUNT(lendings.*) as total_clients')
+            $itemTransfer = Payment::selectRaw('COUNT(*) as total_count, SUM(payments.amount) as total_amount, COUNT(lendings.id) as total_clients')
                         ->join('lendings', 'lendings.id', '=', 'payments.lending_id')
                         ->join('listings', 'listings.id', '=', 'lendings.listing_id')
                         ->whereBetween('payments.date', [$date." 00:00:00", $date." 23:59:59"])
