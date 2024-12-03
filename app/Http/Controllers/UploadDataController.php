1947d536a30779c6e704abc7ac30660c15c6c895
vgualdron <victor.gualdron.r@gmail.com>
2024-12-03 02:48:56 -0500

    add endpoint add payment old

--- app/Http/Controllers/UploadDataController.php
+++ app/Http/Controllers/UploadDataController.php
@@ -7,6 +7,7 @@ use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
