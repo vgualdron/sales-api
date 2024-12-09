3e2cbbc87a6e283531200b3ae09426f99436d19b
vgualdron <victor.gualdron.r@gmail.com>
2024-12-09 15:35:51 -0500

    add func upload data

--- app/Models/Novel.php
+++ app/Models/Novel.php
@@ -65,7 +65,7 @@ class Novel extends Model
         'account_number',
         'account_type_third',
         'account_number_third',
-        'accuont_name_third',
+        'account_name_third',
         'account_active',
         'type_cv',
         'has_letter',
