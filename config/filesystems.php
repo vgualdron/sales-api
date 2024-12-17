3bdbc07f5ec12662c5265ae318fd76af201e9148
vgualdron <victor.gualdron.r@gmail.com>
2024-12-16 22:43:41 -0500

    add fields and actions on red list

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,6 +69,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'users' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/users'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
