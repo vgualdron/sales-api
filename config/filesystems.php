0f5159e7192b906db6ed4ac11c93a04c2cb3327f
vgualdron <victor.gualdron.r@gmail.com>
2024-12-19 22:35:17 -0500

    add field on reddirections table

--- config/filesystems.php
+++ config/filesystems.php
@@ -76,6 +76,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'reddirections' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/reddirections'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
