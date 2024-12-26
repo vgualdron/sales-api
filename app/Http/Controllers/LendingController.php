3d496a9827c73443f4bd15da3267cd254053e936
vgualdron <victor.gualdron.r@gmail.com>
2024-12-26 17:15:53 -0500

    remove permision middleware api user

--- app/Http/Controllers/LendingController.php
+++ app/Http/Controllers/LendingController.php
@@ -104,13 +104,13 @@ class LendingController extends Controller
                         ->whereBetween('lendings.updated_at', [$startDate, $endDate]);
                 });
             })
-            ->where(function ($query) {
+            /* ->where(function ($query) {
                 $query->whereNull('lendings.expense_id') // Si expense_id es null, no se aplica la condición adicional
                     ->orWhere(function ($subQuery) {
                         $subQuery->whereNotNull('lendings.expense_id') // Si expense_id no es null, se verifica files.id
                             ->whereNotNull('files.id');
                     });
-            })
+            }) */
             ->distinct()
             ->orderBy('lendings.type', 'asc')
             ->orderBy('news.id', 'asc')
