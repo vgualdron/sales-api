b3c4f4add751b11e1908f2a704891e8a3011a653
vgualdron <victor.gualdron.r@gmail.com>
2025-01-02 13:58:03 -0500

    add func capital listings

--- app/Models/Capitallisting.php
+++ app/Models/Capitallisting.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Capitallistings extends Model
+class Capitallisting extends Model
 {
     public $table = "capitallistings";
 
