0db499206fab32df2c0995d71ed92789d8bfdf9e
vgualdron <victor.gualdron.r@gmail.com>
2025-01-02 18:52:26 -0500

    add func capital listings

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080', 'https://www.micomercio.com.co', 'https://app-prestamos.micomercio.com.co'],
+    'allowed_origins' => ['http://localhost:9000', 'http://localhost:8080', 'https://www.micomercio.com.co', 'https://app-prestamos.micomercio.com.co'],
 
     'allowed_origins_patterns' => [],
 
