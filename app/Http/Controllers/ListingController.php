6dcbe8f40226e7a79f4600294853ee542a7e6dc0
vgualdron <victor.gualdron.r@gmail.com>
2025-01-03 00:26:18 -0500

    add func capital listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -646,8 +646,8 @@ class ListingController extends Controller
                                 lendings ON lendings.listing_id = listings.id
                             LEFT JOIN
                                 payments p ON lendings.id = p.lending_id
-                                AND MONTH(p.date) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-                                AND YEAR(p.date) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
+                                AND MONTH(p.date) = MONTH(CURRENT_DATE)
+                                AND YEAR(p.date) = YEAR(CURRENT_DATE)
                                 AND p.is_valid = 1
                             WHERE
                                 lendings.listing_id = '. $idList .'
@@ -661,8 +661,8 @@ class ListingController extends Controller
                         FROM
                             capitallistings
                         WHERE
-                            MONTH(created_at) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-                            AND YEAR(created_at) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
+                            MONTH(created_at) = MONTH(CURRENT_DATE)
+                            AND YEAR(created_at) = YEAR(CURRENT_DATE)
                             AND created_at <= CURRENT_DATE();
                         ');
 
