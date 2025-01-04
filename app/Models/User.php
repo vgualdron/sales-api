d86f8485d80fbdb1ed4ec3d2cd49a8e7da362815
vgualdron <victor.gualdron.r@gmail.com>
2025-01-04 02:58:10 -0500

    'init'

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable, HasRoles;
-    
+
     public $table = "users";
 
     protected $guard_name = 'api';
