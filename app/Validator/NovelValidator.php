4e6b66bb8b77603fdc50a0f9758a53635331ba2d
vgualdron <victor.gualdron.r@gmail.com>
2025-01-22 15:44:47 -0500

    add passport

--- app/Validator/NovelValidator.php
+++ app/Validator/NovelValidator.php
@@ -14,7 +14,7 @@
 
         private function rules(){
             return[
-                'documentNumber' => 'unique:news,document_number,'.$this->data['id'],
+                'document_number' => 'unique:news,document_number,'.$this->data['id'],
                 'name' => 'required|min:5|max:50',
                 'phone' => 'required|min:5|max:15|unique:news,phone,'.$this->data['id'],
             ];
@@ -22,9 +22,9 @@
 
         private function messages(){
             return [
-                'documentNumber.unique' => 'El número de documento "'.$this->data['documentNumber'].'" ya se encuentra registrado',
-                'documentNumber.min' => 'El número de documento debe tener un mínimo de 5 caracteres',
-                'documentNumber.max' => 'El número de documento debe tener un máximo de 15 caracteres',
+                'document_number.unique' => 'El número de documento "'.$this->data['document_number'].'" ya se encuentra registrado',
+                'document_number.min' => 'El número de documento debe tener un mínimo de 5 caracteres',
+                'document_number.max' => 'El número de documento debe tener un máximo de 15 caracteres',
                 'name.required' => 'El nombre es requerido',
                 'name.min' => 'El nombre debe tener un mínimo de 5 caracteres',
                 'name.max' => 'El nombre debe tener un máximo de 50 caracteres',
