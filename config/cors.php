106db89f816e5f92c5e5f55ad59607a319525dab
vgualdron <victor.gualdron.r@gmail.com>
2025-01-22 12:33:50 -0500

    add passport

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:9000', 'http://localhost:8080', 'https://saac.com.co', 'https://saac.com.co/api/public/api'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
