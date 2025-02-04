10e8ace384f5d3b506b40ec9364365894367cca1
vgualdron <victor.gualdron.r@gmail.com>
2025-02-04 12:04:37 -0500

    add pqr mail

--- app/Mail/PqrEmail.php
+++ app/Mail/PqrEmail.php
@@ -37,7 +37,7 @@ class PqrEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.pqr',
+            view: 'pqr',
         );
     }
 
