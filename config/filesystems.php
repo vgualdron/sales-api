c5adc555a07b7bc4b9c1e626e80775d56d4f3556
vgualdron <victor.gualdron.r@gmail.com>
2025-02-07 21:55:18 -0500

    sadas

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'shops' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/shops'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
